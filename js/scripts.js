b1b022bc6258234ef1894475433096765433b48b
Caitlin Hines and Katy Daviscourt <caitlin@>
2017-01-19 13:24:47 -0800

    write code in javascript for full ones column plus some tens column js that doesnt work yet

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,60 @@ var bigRomanConvert = function(number) {
       return "Please enter a number less than 4,000"
     }
 
-    return onesColumn(number);
+    return Columns(number);
 
   };
 
-  var onesColumn = function(number) {
+  var Columns = function(number) {
     var numberInputArray = number.split("");
-    var onesColumnIndex = integers.indexOf(number[numberInputArray.length-1])
-    if (onesColumnIndex === 0) {
-      return "The result is:";
+    var onesColumn = function(number) {
+      var onesColumnIndex = integers.indexOf(number[numberInputArray.length-1])
+      if (onesColumnIndex === 0) {
+        return "_";
+      } else if (onesColumnIndex === 1) {
+        return "I"
+      } else if (onesColumnIndex === 2) {
+        return "II"
+      } else if (onesColumnIndex === 3) {
+        return "III"
+      } else if (onesColumnIndex === 4) {
+        return "IV"
+      } else if (onesColumnIndex === 5) {
+        return "V"
+      } else if (onesColumnIndex === 6) {
+        return "VI"
+      } else if (onesColumnIndex === 7) {
+        return "VII"
+      } else if (onesColumnIndex === 8) {
+        return "VIII"
+      } else if (onesColumnIndex === 9) {
+        return "IX"
+      }
+    };
+    var tensColumn = function(number) {
+      var numberInputArray = number.split("");
+      var tensColumnIndex = integers.indexOf(number[numberInputArray.length-2])
+      if (tensColumnIndex === 0) {
+        return "_";
+      } else if (tensColumnIndex === 1) {
+        return "X"
+      } else if (tensColumnIndex === 2) {
+        return "XX"
+      } else if (tensColumnIndex === 3) {
+        return "XXX"
+      } else if (tensColumnIndex === 4) {
+        return "XL"
+      } else if (tensColumnIndex === 5) {
+        return "L"
+      } else if (tensColumnIndex === 6) {
+        return "LX"
+      } else if (tensColumnIndex === 7) {
+        return "LXX"
+      } else if (tensColumnIndex === 8) {
+        return "LXXX"
+      } else if (tensColumnIndex === 9) {
+        return "XC"
+      }
     };
   };
 
