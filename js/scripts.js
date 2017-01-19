38d2623d7a795db975cdbd8db0c28bf5468c80b0
Caitlin Hines and Katy Daviscourt <katy@>
2017-01-19 10:46:25 -0800

    first spec javascript finished

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,31 @@ var integers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 var bigRomanConvert = function(number) {
 
-  var numberInputArray = numberInput.split("");
+  var numberInputArray = number.split("");
+  var myAnswer = ""
   var number = true;
+
   numberInputArray.forEach(function(element) {
     var intquery = integers.indexOf(element);
-    if (intquery = -1) {
+    if (intquery === -1) {
       number = false;
-    }
-  })
+      myAnswer = "please return only numbers";
+    };
+  // if(number = false) {
+  //     return "Please enter only numbers";
+    // };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
+  }); return myAnswer;
+};
 
 
 //Front end logic
 $(document).ready(function(){
-  ("form#convert").submit(function(event) {
+  $("form#convert").submit(function(event) {
     event.preventDefault();
-    var numberInput = parseInt($("input#numberEntry").val());
+    var numberInput = $("input#numberEntry").val();
+
+    $(".result").text(bigRomanConvert(numberInput));
 
   });
 });
