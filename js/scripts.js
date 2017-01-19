38fdea28dda786c0de8b76db86699de8670bedd3
Caitlin Hines and Katy Daviscourt <caitlin@>
2017-01-19 10:53:00 -0800

    simplify first spec in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,17 @@ var integers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var bigRomanConvert = function(number) {
 
   var numberInputArray = number.split("");
-  var myAnswer = ""
   var number = true;
 
   numberInputArray.forEach(function(element) {
     var intquery = integers.indexOf(element);
     if (intquery === -1) {
       number = false;
-      myAnswer = "please return only numbers";
     };
-  // if(number = false) {
-  //     return "Please enter only numbers";
-    // };
-
-  }); return myAnswer;
+  });
+  if(!number) {
+      return "Please enter only numbers";
+    };
 };
 
 
